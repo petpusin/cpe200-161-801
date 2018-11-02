5f7e454c5873faa09015478a95539197cb49528b
Chin Isradisaikul <chinawat@cs.cornell.edu>
2018-11-02 13:47:19 +0700

    Added skeleton NodeFactory.

---
+++ ArithEval/ArithEval/INodeFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithEval
+{
+    interface INodeFactory
+    {
+        Expr Number(int n);
+
+        Expr Add(Expr e1, Expr e2);
+
+        Expr Subtract(Expr e1, Expr e2);
+
+        Expr Multiply(Expr e1, Expr e2);
+
+        Expr Negate(Expr e1);
+    }
+}
