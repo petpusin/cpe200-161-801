7ac3109cd0709e0922b7966f08f5a3a2e6301241
Chin Isradisaikul <chinawat@cs.cornell.edu>
2018-11-02 13:20:49 +0700

    Set up project.

---
+++ ArithEval/ArithEval/Expr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithEval
+{
+    interface Expr
+    {
+        /// <summary>
+        /// Return the string representation of this expression.
+        /// </summary>
+        /// <returns></returns>
+        string ToString();
+    }
+}
